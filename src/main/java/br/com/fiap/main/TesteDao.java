138b12a7e7b27ee3a4b0552b8d06079a8b33fd04
Matheus Barbosa Gonzales <matheusbarbosa.gonzales@gmail.com>
2025-06-04 23:22:06 -0300

    descobri que o erro do parsing estava no banco. Get e Post criados

--- src/main/java/br/com/fiap/main/TesteDao.java
+++ src/main/java/br/com/fiap/main/TesteDao.java
@@ -11,18 +11,12 @@ public class TesteDao
 {
     public static void main(String[] args) throws SQLException, ClassNotFoundException
     {
-        //Chamado chamdo = new Chamado(-23.7199405, -46.6125048);
-        //ChamadoDAO chamadoDao = new ChamadoDAO();
-        //chamadoDao.inserir(chamdo);
         ChamadoDAO chamadoDao = new ChamadoDAO();
-        //chamadoDao.atualizarStatus(1);
         List<Chamado> chamados = chamadoDao.selecionar();
         for(Chamado chamado : chamados)
         {
             System.out.println(chamado);
         }
-
-
     }
 
 
